1d4632fcd5a47c7bb23a71a8373c6ad81196da65
ishitdmg2004 <97721951+ishitdmg2004@users.noreply.github.com>
2022-11-22 04:16:55 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,32 @@
+let carts = document.querySelectorAll('.add-cart');
+
+for(let i=0; i< carts.length; i++){
+    carts[i].addEventListener('click', () =>{
+        cartNumbers();
+    })
+}
+
+function onLoadCartNumbers(){
+    let productNumbers=localStorage.getItem('cartNumbers');
+    if(productNumbers){
+        document.querySelector('cart.span').textContent = productNumbers;  
+    }
+}
+
+function cartnumbers(){
+    let productNumbers= localStorage.getItem('cartNumbers');
+
+    productNumbers=parseInt(productNumbers);
+    
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('cart.span').textContent = productNumbers + 1;
+    }
+    else{
+        localStorage.setItem('cartNumbers',1);
+        document.querySelector('cart.span').textContent = 1;
+    }
+}
+
+onLoadCartNumbers();
+    
