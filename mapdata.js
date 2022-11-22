89eec86d749d0c3e18b5390797226f149bc3323e
ishitdmg2004 <97721951+ishitdmg2004@users.noreply.github.com>
2022-11-22 13:47:11 +0530

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,348 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "520", //'700' or 'responsive'
+    
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "rgb(149, 66, 6)",
+    state_hover_color: "#DAA06D",
+    border_color: "black",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 15,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 25,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "blue",
+    label_hover_color: "green",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.1,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "18px/1.5 sans-serif",
+    popup_nocss: "no",
+    
+    
+    //Advanced settings
+    div: "map og",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      description: "Coconut Prawn Curry OR Macher Jhol",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+      
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      description: "Pesarattu AND Gutti Vankaya Koora",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+      
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      description: "Pehak OR Pasa",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "4": {
+      name: "Assam",
+      description: "Pitika AND Pitha",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "5": {
+      name: "Bihar",
+      description: "Litti Chokha AND Khaja",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+      //back_image:url('C:\Users\ishit\OneDrive\Desktop\P.I.N.D\Images States\qutub minar.jpg'),
+    },
+    "6": {
+      name: "Chandigarh",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      description: "Aamat AND Khurma",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      description: "Jetty Roll AND Undhiyu",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "9": {
+      name: "Daman And Diu",
+      description: "Dhansak AND Ubadiyu",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "10": {
+      name: "Delhi",
+      description: "Butter Chicken AND Chhole Bhature",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "11": {
+      name: "Goa",
+      description: "Poee AND Feni",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "12": {
+      name: "Gujarat",
+      description: "Dabeli AND Fafda Jalebi",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "13": {
+      name: "Haryana",
+      description: "Kachri ki Sabzi AND Bathua Raita",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      description: "Siddu AND Aktori",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "16": {
+      name: "Jharkhand",
+      description: "Handia AND Rugra",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "17": {
+      name: "Karnataka",
+      description: "Mysore Bonda AND Chiroti",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "18": {
+      name: "Kerala",
+      description: "Erissery AND Ela Sadya",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "19": {
+      name: "Lakshadweep",
+      description: "Mus Kawaab AND Batla Appam",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      description: "Palak Puri AND Bhutte Ka Kees",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "21": {
+      name: "Maharashtra",
+      description: "Malvani Cuisine OR Thalipeeth",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "22": {
+      name: "Manipur",
+      description: "Chamthong AND Chagem Pomba",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "23": {
+      name: "Meghalaya",
+      description: "Pumaloi AND Jadoh",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "24": {
+      name: "Mizoram",
+      description: "Bai AND Bamboo Shoot Fry",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "25": {
+      name: "Nagaland",
+      description: "Samathu AND Akini",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "26": {
+      name: "Orissa",
+      description: "Dalma AND Gupchup",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+      
+    },
+    "27": {
+      name: "Puducherry",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "28": {
+      name: "Punjab",
+      description: "Amritsari Kulcha OR Makki ki Roti/Sarson Ka Saag",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+      
+    },
+    "29": {
+      name: "Rajasthan",
+      description: "Dal Batti Churma OR Gatte Ki Sabzi",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "30": {
+      name: "Sikkim",
+      description: "Gundruk AND Dhindo",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      description: "Rasam AND Pongal",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "32": {
+      name: "Tripura",
+      description: "Kosoi Bwtwi AND Bhangui",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      description: "Lucknowi Biryani AND Malaiyo",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+      
+    },
+    "34": {
+      name: "Uttaranchal",
+      description: "Baadi AND Dubuk",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "35": {
+      name: "West Bengal",
+      description: "Shukto AND Sandesh",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      description: "Tabak Maaz AND Nadir Monji",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "37": {
+      name: "Telangana",
+      description: "Hyderabadi Biryani AND Thunti Koora",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "38": {
+      name: "Ladakh",
+      description: "Thukpa AND Chhurpe",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    }
+  },
+  
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
+
+      
